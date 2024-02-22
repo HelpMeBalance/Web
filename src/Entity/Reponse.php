f3194932af02e4ff147762197fdcad8bd89f2e6e
fadiamori <fadiamorri2002@gmail.com>
2024-02-22 20:23:10 +0100

    aaaa

--- src/Entity/Reponse.php
+++ src/Entity/Reponse.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ReponseRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -21,6 +23,14 @@ class Reponse
     #[ORM\ManyToOne(inversedBy: 'reponse')]
     #[Assert\NotBlank(message: 'Please enter a question.')]
     private ?Question $question = null;
+
+    #[ORM\ManyToMany(targetEntity: Formulaire::class, mappedBy: 'Reponse')]
+    private Collection $formulaires;
+
+    public function __construct()
+    {
+        $this->formulaires = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -47,6 +57,33 @@ class Reponse
     public function setQuestion(?Question $question): static
     {
         $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Formulaire>
+     */
+    public function getFormulaires(): Collection
+    {
+        return $this->formulaires;
+    }
+
+    public function addFormulaire(Formulaire $formulaire): static
+    {
+        if (!$this->formulaires->contains($formulaire)) {
+            $this->formulaires->add($formulaire);
+            $formulaire->addReponse($this);
+        }
+
+        return $this;
+    }
+
+    public function removeFormulaire(Formulaire $formulaire): static
+    {
+        if ($this->formulaires->removeElement($formulaire)) {
+            $formulaire->removeReponse($this);
+        }
 
         return $this;
     }
